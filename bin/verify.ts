488cb241ab05bb6e0900bbb140a8693a2ab504e4
Shailesh <kochhar@users.noreply.github.com>
2019-02-18 16:04:42 +0530

    Consolidate on a standard set of prefixes.

--- bin/verify.ts
+++ bin/verify.ts
@@ -8,23 +8,38 @@ import yargs = require('yargs');
 import { LinkedDataSignature } from '../lib/signatures';
 import { RsaSignature2018 } from '../lib/suites';
 
+const PREFIX = {
+  OPENBADGES: 'ob',
+  SKILLCRED: 'scd',
+  SECURITY: 'sec',
+  SCHEMA: 'schema',
+  DUBLINCORE: 'dc'
+}
 
-async function signCredentialInFile(filename: string, outFile: string, options: object) {
-    var keyFile = options.keyFile;
-    var keyId = options.keyId;
+const COMPACT_CONTEXT = {
+  'ob': 'https://w3id.org/openbadges/v2#',
+  'scd': 'https://skillcredentialspec.org/v1/',
+  'sec': 'https://w3id.org/security/v1#',
+  'schema': 'http://schema.org/'
+}
+
+
+async function signCredentialInFile(filename: string, outFile: string, options: {[k: string]: any}) {
+    var keyFile = options['keyFile'];
+    var keyId = options['keyId'];
 
     const fileContents = fs.readFileSync(filename, 'utf8');
     var document = JSON.parse(fileContents);
     const context = document['@context'];
     delete document['@context'];
-    var credential = await jsonld.compact(document, context);
+    var credential = await jsonld.compact(document, COMPACT_CONTEXT, {expandContext: context});
 
     const publicKeyPem = fs.readFileSync(keyFile+'.pub', 'utf8');
-    credential['ob:badge']['ocd:awardedBy']['ocd:publicKey'] = {
+    credential['ob:badge']['ob:issuer']['sec:publicKey'] = {
         '@id': keyId,
         '@type': 'ob:CryptographicKey',
-        'sec:owner': credential['ob:badge']['ocd:awardedBy']['@id'],
-        'publicKeyPem': publicKeyPem
+        'sec:owner': credential['ob:badge']['ob:issuer']['@id'],
+        'sec:publicKeyPem': publicKeyPem
     };
 
     const privateKeyContents = fs.readFileSync(keyFile, 'utf8');
@@ -34,7 +49,7 @@ async function signCredentialInFile(filename: string, outFile: string, options: 
 
     const signature = new LinkedDataSignature(new RsaSignature2018());
     const signedCredential = signature.sign(credential, privateKey, keyId);
-    process.stdout.write(JSON.stringify(signedCredential, 2)+"\n")
+    process.stdout.write(JSON.stringify(signedCredential, null, 2)+"\n")
     process.stderr.write("Credential created.\n");
 }
 
@@ -43,9 +58,11 @@ async function verifyCredentialInFile (filename: string, trace?: boolean): Promi
     var document = JSON.parse(fileContents);
     const context = document['@context'];
     delete document['@context'];
-    var signedCredential = await jsonld.compact(document, context);
+    var signedCredential = await jsonld.compact(document, COMPACT_CONTEXT, {expandContext: context});
+
     var signature = new LinkedDataSignature(new RsaSignature2018());
-    var verified = await signature.verify(signedCredential);
+    var verified = await signature.verify(signedCredential,
+                                          signedCredential["ob:badge"]["ob:issuer"]["sec:publicKey"]);
     if (!verified) {
         throw new Error("Signature verification failed");
     } else {
